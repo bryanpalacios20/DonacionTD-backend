34e48335bc47f3b900b0de29ed3859272fe9db76
bryanpalacios20 <80603906+bryanpalacios20@users.noreply.github.com>
2022-06-11 14:04:24 -0500

    Update DonacionApplicationTests.java
    corrigiendo 2

--- src/test/java/com/donacion/DonacionApplicationTests.java
+++ src/test/java/com/donacion/DonacionApplicationTests.java
@@ -3,7 +3,7 @@ package com.donacion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DonacionApplication.class)
+@SpringBootTest(classes = DonacionApplicationTests.class)
 class DonacionApplicationTests {
 
 	@Test
